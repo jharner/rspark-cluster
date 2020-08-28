a4da764a307a160ba023833ebd1eab2ab6730f61
Chris Grant <chrisgrant.datascience@gmail.com>
2020-08-28 18:20:16 -0400

    Updated to Hadoop 2.10.0, Spark 3.0.0.  Also, updated tests.

--- sparrow-test.R
+++ sparrow-test.R
@@ -5,7 +5,7 @@ library(dplyr)
 sc <- spark_connect(master = "spark://master:7077")
 # sc <- spark_connect(master = "local")
 ###, config = list("sparklyr.shell.driver-memory" = "6g"))
-data <- data.frame(y = runif(10^3, 0, 1))
+data <- data.frame(y = runif(10^5, 0, 1))
 
 ## Benchmark Data copy_to()
 microbenchmark::microbenchmark(
@@ -22,10 +22,10 @@ microbenchmark::microbenchmark(
   times = 10
 ) %T>% print() %>% ggplot2::autoplot()
 
-spark_disconnect(sc)
+# spark_disconnect(sc)
 
-# sc <- spark_connect(master = "local")
-sc <- spark_connect(master = "spark://master:7077")
+
+# sc <- spark_connect(master = "spark://master:7077")
 
 # Benchmark collect()
 microbenchmark::microbenchmark(
@@ -39,39 +39,18 @@ microbenchmark::microbenchmark(
   },
   times = 10
 ) %T>% print() %>% ggplot2::autoplot()
-spark_disconnect(sc)
-
-# sc <- spark_connect(master = "local")
-sc <- spark_connect(master = "spark://master:7077")
 
 ## Benchmark data transformation
 microbenchmark::microbenchmark(
   setup = library(arrow),
   arrow_on = {
-    sample_n(sparklyr_df, 10^2) %>% spark_apply(~ .x / 2) %>% count()
+    sample_n(sparklyr_df, 10^4) %>% spark_apply(~ .x / 2) %>% count()
   },
   arrow_off = {
     if ("arrow" %in% .packages()) detach("package:arrow")
-    sample_n(sparklyr_df, 10^2) %>% spark_apply(~ .x / 2) %>% count()
+    sample_n(sparklyr_df, 10^4) %>% spark_apply(~ .x / 2) %>% count()
   },
   times = 10
 ) %T>% print() %>% ggplot2::autoplot()
-spark_disconnect(sc)
-
-# Faithful dataset
-#microbenchmark::microbenchmark(
-#  setup = library(arrow),
-#  arrow_on = {
-#    sample_n(sparklyr_df, 10^5) %>% spark_apply(~ .x / 2) %>% count()
-#  },
-#  arrow_off = {
-#    if ("arrow" %in% .packages()) detach("package:arrow")
-#    sample_n(sparklyr_df, 10^5) %>% spark_apply(~ .x / 2) %>% count()
-#  },
-#  times = 10
-#) %T>% print() %>% ggplot2::autoplot()
-#faithful_tbl <- copy_to(sc, faithful, "faithful_sdf", overwrite = TRUE)
-#src_tbls(sc)
-
 
 spark_disconnect(sc)
