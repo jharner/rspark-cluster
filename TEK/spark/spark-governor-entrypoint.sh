9d3859477937fb3e8e9db0bee8480b586eb8c673
Chris Grant <chrisgrant.datascience@gmail.com>
2020-09-14 16:58:08 -0400

    Created entrypoint scripts either from existing start-up scripts or incorporating direct CMD's to a .sh file.

---
+++ TEK/spark/spark-governor-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#while true; do sleep 1000; done
+
+export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
+export HADOOP_HOME=/opt/hadoop
+export HIVE_HOME=/opt/hive
+export SPARK_HOME=/opt/spark
+export HADOOP=/opt/hadoop/bin/hadoop
+
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master
+
+
